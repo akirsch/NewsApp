aab0ca9bced31e09fd2841108a7c8bde23f1a006
amk87 <adamkirsch1@gmail.com>
2018-05-23 22:29:01 +0300

    feat - fully functioning preference screen

--- app/src/main/java/com/example/android/newsapp/MainActivity.java
+++ app/src/main/java/com/example/android/newsapp/MainActivity.java
@@ -4,9 +4,12 @@ import android.app.LoaderManager;
 import android.content.Context;
 import android.content.Intent;
 import android.content.Loader;
+import android.content.SharedPreferences;
 import android.graphics.drawable.Drawable;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.net.Uri;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -28,7 +31,7 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
     /**
      * target url for a Guardian API query
      */
-    private static final String JSON_RESPONSE = "https://content.guardianapis.com/search?show-fields=thumbnail,trailText,headline,byline&api-key=49c021e8-1aba-47fe-887d-e7ff6cd5888b";
+    private static final String JSON_RESPONSE = "https://content.guardianapis.com/search?";
 
     // initialize String constant to store value of private api key the the Guardian Api
     private static final String API_KEY = "49c021e8-1aba-47fe-887d-e7ff6cd5888b";
@@ -127,7 +130,33 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
     public Loader<ArrayList<NewsItem>> onCreateLoader(int i, Bundle bundle) {
         // Create a new loader for the given URL
         Log.v(LOG_TAG, "onCreateLoader called");
-        return new NewsItemLoader(this, JSON_RESPONSE);
+
+        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
+
+        String topic = sharedPrefs.getString(
+                getString(R.string.settings_display_by_topic_key),
+                getString(R.string.settings_display_by_topic_default));
+
+        String dateOrder = sharedPrefs.getString(
+                getString(R.string.settings_order_by_date_key),
+                getString(R.string.settings_order_by_date_default));
+
+        // parse breaks apart the URI string that's passed into its parameter
+        Uri baseUri = Uri.parse(JSON_RESPONSE);
+
+        // buildUpon prepares the baseUri that we just parsed so we can add query parameters to it
+        Uri.Builder uriBuilder = baseUri.buildUpon();
+
+        // Append query parameter and its value. For example, the `format=geojson`
+        uriBuilder.appendQueryParameter("format", "json");
+        uriBuilder.appendQueryParameter("section", topic);
+        uriBuilder.appendQueryParameter("show-fields", getResources().getString(R.string.show_fields_values));
+        uriBuilder.appendQueryParameter("order-by", dateOrder);
+        uriBuilder.appendQueryParameter("api-key", API_KEY);
+
+        Log.v(LOG_TAG, uriBuilder.toString());
+
+        return new NewsItemLoader(this, uriBuilder.toString());
     }
 
     @Override
